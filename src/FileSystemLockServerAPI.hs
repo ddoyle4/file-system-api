92686582f77eb76ee241caadd0195b89aeba5ab2
David Doyle <ddoyle3@tcd.ie>
2016-12-04 20:34:19 +0000

    adding API for locking service

---
+++ src/FileSystemLockServerAPI.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module FileSystemLockServerAPI where
+
+
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Data.Bson.Generic
+import           GHC.Generics
+import           Servant
+import qualified Crypto.Cipher.AES as CCA
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import Crypto.Hash.MD5
+
+
+
+
+data LockFileReq = LockFileReq  { lockVirtPath :: String
+                                } deriving (Generic, ToJSON, FromJSON,FromBSON, Show)
+
+data UnlockFileReq = UnlockFileReq  { unlockVirtPath :: String
+                                    } deriving (Show, Generic, ToJSON, FromJSON, FromBSON, ToBSON, Read)
+
+deriving instance FromBSON String  -- we need these as BSON does not provide
+deriving instance ToBSON   String
+
+
+type LockServiceAPI = "lockFile"                    :> ReqBody '[JSON] LockFileReq :> Post '[JSON] Bool
+                      :<|> "unlockFile"             :> ReqBody '[JSON] UnlockFileReq :> Post '[JSON] Bool
+
